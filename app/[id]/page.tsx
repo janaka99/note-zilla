93a9293b121eaa2d940e2691f69a4f6efe21f9c3
janaka99 <janakachamith88@gmail.com>
2024-03-31 15:16:50 +0530

    first commit

--- app/[id]/page.tsx
+++ app/[id]/page.tsx
@@ -3,10 +3,12 @@ import ManageNote from "@/components/form/Note/ManageNote";
 import useGetApi from "@/hooks/useGetApi";
 import { usePathname, useRouter } from "next/navigation";
 import React, { useContext, useEffect, useState } from "react";
-import { Page } from "@/components/HOC/isAuth/IsAuthenticated";
 import NewNote from "@/components/form/Note/NewNote";
 import { NoteContext } from "@/context/NoteContext/NoteContext";
 import SideBar from "@/containers/Sidebar/SideBar";
+import { AuthContext } from "@/context/AuthContext/AuthContext";
+import Loading from "@/components/UI/Loading/Loading";
+import { FaArrowRight } from "react-icons/fa6";
 
 type Note = {
   title: string;
@@ -16,6 +18,8 @@ type Note = {
 
 const page = () => {
   const { getNotes, notes: allNotes, isNotesLoading } = useContext(NoteContext);
+  const { isAuthenticated, checkUser, isLoading } = useContext(AuthContext);
+  const path = usePathname();
 
   const [note, setNote] = useState<Note>({
     title: "",
@@ -47,6 +51,45 @@ const page = () => {
     getNote();
     getNotes();
   }, [noteId]);
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (!isAuthenticated) {
+    if (path != "/") {
+      return (
+        <div className=" min-h-svh flex flex-col justify-center items-center text-primary-500">
+          <h1 className="text-2xl uppercase">
+            Page you are looking for is not found
+          </h1>
+          <p className="font-light">Maybe you are not logged in</p>
+          <a
+            className="mt-4 texy-2xl flex gap-3 items-center  border-b border-b-background-500 hover:border-primary-500 transition-all duration-200"
+            href="/auth/login"
+          >
+            Login Now
+            <FaArrowRight />
+          </a>
+        </div>
+      );
+    } else {
+      return (
+        <div className=" min-h-svh flex flex-col justify-center items-center text-primary-500 gap-3">
+          <h1 className="text-4xl uppercase">Welcome to Note Zilla</h1>
+          <h1 className="text-2xl uppercase font-light">
+            Your Ultimate Note-Taking Companion
+          </h1>
+          <a
+            className="mt-4 texy-2xl flex gap-3 items-center  border-b border-b-background-500 hover:border-primary-500 transition-all duration-200"
+            href="/auth/login"
+          >
+            Login Now
+            <FaArrowRight />
+          </a>
+        </div>
+      );
+    }
+  }
 
   if (allNotes.length == 0 && !isNotesLoading) {
     return (
@@ -71,4 +114,4 @@ const page = () => {
   );
 };
 
-export default Page(page);
+export default page;
--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,59 @@
 "use client";
-import { Page } from "@/components/HOC/isAuth/IsAuthenticated";
+
 import Loading from "@/components/UI/Loading/Loading";
 import NewNote from "@/components/form/Note/NewNote";
+import { AuthContext } from "@/context/AuthContext/AuthContext";
 import { NoteContext } from "@/context/NoteContext/NoteContext";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 import React, { useContext, useEffect, useState } from "react";
+import { FaArrowRight } from "react-icons/fa6";
 
 const page = () => {
   const { push } = useRouter();
   const { getNotes, notes, isNotesLoading } = useContext(NoteContext);
-
+  const { isAuthenticated, checkUser, isLoading } = useContext(AuthContext);
+  const path = usePathname();
   useEffect(() => {
     getNotes();
   }, []);
 
-  if (isNotesLoading) {
+  if (isNotesLoading || isLoading) {
     return <Loading />;
+  }
+  if (!isAuthenticated) {
+    if (path != "/") {
+      return (
+        <div className=" min-h-svh flex flex-col justify-center items-center text-primary-500">
+          <h1 className="text-2xl uppercase">
+            Page you are looking for is not found
+          </h1>
+          <p className="font-light">Maybe you are not logged in</p>
+          <a
+            className="mt-4 texy-2xl flex gap-3 items-center  border-b border-b-background-500 hover:border-primary-500 transition-all duration-200"
+            href="/auth/login"
+          >
+            Login Now
+            <FaArrowRight />
+          </a>
+        </div>
+      );
+    } else {
+      return (
+        <div className=" min-h-svh flex flex-col justify-center items-center text-primary-500 gap-3">
+          <h1 className="text-4xl uppercase">Welcome to Note Zilla</h1>
+          <h1 className="text-2xl uppercase font-light">
+            Your Ultimate Note-Taking Companion
+          </h1>
+          <a
+            className="mt-4 texy-2xl flex gap-3 items-center  border-b border-b-background-500 hover:border-primary-500 transition-all duration-200"
+            href="/auth/login"
+          >
+            Login Now
+            <FaArrowRight />
+          </a>
+        </div>
+      );
+    }
   }
 
   if (notes.length > 0) {
@@ -32,4 +70,4 @@ const page = () => {
   }
 };
 
-export default Page(page);
+export default page;
